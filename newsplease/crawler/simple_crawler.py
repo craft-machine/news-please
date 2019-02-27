60c190964955eccf6c41d9967cd22ee73f543f47
TheML9I <s.khaletskiy@me.com>
2019-02-27 19:25:01 +0300

    Added the ability to pass custom headers for SimpleCrawler

--- newsplease/crawler/simple_crawler.py
+++ newsplease/crawler/simple_crawler.py
@@ -6,27 +6,30 @@ from six.moves import urllib
 
 class SimpleCrawler(object):
     _results = {}
+    _default_headers = {'User-Agent': 'Mozilla/5.0'}
 
     @staticmethod
-    def fetch_url(url, timeout=None):
+    def fetch_url(url, timeout=None, headers=None):
         """
         Crawls the html content of the parameter url and returns the html
         :param url:
         :param timeout: in seconds, if None, the urllib default is used
+        :param headers: if None, default headers will be used
         :return:
         """
-        return SimpleCrawler._fetch_url(url, False, timeout=timeout)
+        headers = headers if headers is not None else copy.copy(SimpleCrawler._default_headers)
+        return SimpleCrawler._fetch_url(url, False, timeout=timeout, headers=headers)
 
     @staticmethod
-    def _fetch_url(url, is_threaded, timeout=None):
+    def _fetch_url(url, is_threaded, timeout=None, headers=None):
         """
         Crawls the html content of the parameter url and saves the html in _results
         :param url:
         :param is_threaded: If True, results will be stored for later processing by the fetch_urls method. Else not.
         :param timeout: in seconds, if None, the urllib default is used
+        :param headers: Request headers
         :return: html of the url
         """
-        headers = {'User-Agent': 'Mozilla/5.0'}
         req = urllib.request.Request(url, None, headers)
         html = urllib.request.urlopen(req, data=None, timeout=timeout).read()
 
@@ -36,14 +39,16 @@ class SimpleCrawler(object):
         return html
 
     @staticmethod
-    def fetch_urls(urls, timeout=None):
+    def fetch_urls(urls, timeout=None, headers=None):
         """
         Crawls the html content of all given urls in parallel. Returns when all requests are processed.
         :param urls:
         :param timeout: in seconds, if None, the urllib default is used
+        :param headers: if None, default headers will be used
         :return:
         """
-        threads = [threading.Thread(target=SimpleCrawler._fetch_url, args=(url, True, timeout)) for url in urls]
+        headers = headers if headers is not None else copy.copy(SimpleCrawler._default_headers)
+        threads = [threading.Thread(target=SimpleCrawler._fetch_url, args=(url, True, timeout, headers)) for url in urls]
         for thread in threads:
             thread.start()
         for thread in threads:
